fc8132725f0f6934b45a93e1b408c670599c6b56
Justin Searls <searls@gmail.com>
2011-07-09 19:47:31 -0400

    figured out how to pull out the css

--- contentscript.js
+++ contentscript.js
@@ -1,31 +1,15 @@
 (function($) {
   var $source, $exit, $originals, 
-    doc = document.documentElement, 
-    css = {
-      'position':'absolute',
-      'top':0,
-      'left':0,
-      'margin':0,
-    };
+    doc = document.documentElement;
   
   var init = function() {
     var sourceHtml = $('<div/>').text(doc.outerHTML).html().replace(/(\t|  )/g,"&nbsp;&nbsp;").split('\n').join('<br/>');
     $originals = $(doc).children().detach();
-
-    $source = $('<div><div>').css($.extend({  
-      'background-color':'#fff',  
-      'width':'100%',
-      'height':'100%',
-      'padding': '30px 10px 10px 30px',
-      'font-family': 'Courier New, monospace'
-    },css)).html(sourceHtml).appendTo(doc);
+    $source = $('<div class="absolute source"><div>').html(sourceHtml).appendTo(doc);
   }
   
   var renderExitButton = function() {
-    $exit = $('<a href="#" class="close-source"><img src="'+chrome.extension.getURL("x.png")+'"/></a>').css($.extend({
-      'z-index': 9999,
-      'padding': '5px 0px 0px 5px'
-    },css)).insertBefore($source);    
+    $exit = $('<a href="#" class="absolute exit"><img src="'+chrome.extension.getURL("x.png")+'"/></a>').insertBefore($source);    
   };
   
   var restorePage = function(e) {
@@ -35,15 +19,16 @@
     $originals.appendTo(doc);
   };
 
-  $('.close-source').live('click',restorePage);
-  $(document.documentElement).keyup(function(e) {
-    if(e.keyCode === 27) {
-      restorePage(e);
-    }
+  $(function() {
+    init();
+    renderExitButton();
+    $('.exit').live('click',restorePage);    
+    $(document.documentElement).keyup(function(e) {
+      if(e.keyCode === 27) {
+        restorePage(e);
+      }
+    });    
   });
-
-  init();
-  renderExitButton();
 
   chrome.extension.sendRequest({}, function(response) {});
 })(jQuery);
