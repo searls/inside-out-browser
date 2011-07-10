bfe41e8631faf96f740b261332a83d2a1a2367d0
Justin Searls <searls@gmail.com>
2011-07-09 20:46:01 -0400

    still sloppy but it renders links

--- contentscript.js
+++ contentscript.js
@@ -3,20 +3,27 @@
   
   var init = function() {
     doc = document.documentElement;
-    var sourceHtml = $('<div/>').text(doc.outerHTML).html().replace(/(\t|  )/g,"&nbsp;&nbsp;").split('\n').join('<br/>');
+    var sourceHtml = renderLinks($('<div/>').text(doc.outerHTML).html().replace(/(\t|  )/g,"&nbsp;&nbsp;").split('\n').join('<br/>'));
+    console.log(sourceHtml);
     $originals = $(doc).children().hide();
     $source = $('<div class="absolute source"><div>').html(sourceHtml).appendTo(doc);
   }
   
   var renderExitButton = function() {
-    $exit = $('<a href="#" class="absolute exit"><img src="'+chrome.extension.getURL("x.png")+'"/></a>').insertBefore($source);    
+    var url = chrome.extension.getURL("x.png");
+    $exit = $('<a href="#" class="absolute exit"><img src="'+url+'"/></a>').insertBefore($source);    
+  };
+  
+  var renderLinks = function(source) {
+    var p = /\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s"()<>]+|\(([^\s"()<>]+|(\([^\s"()<>]+\)))*\))+(?:\(([^\s"()<>]+|(\([^\s"()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi;
+    return source.replace(p, "<a href=\"$&\">$&</a>");
   };
   
   var restorePage = function(e) {
     e.preventDefault();
     $source.remove();
     $exit.remove();
-    $originals.attr('style','');
+    $originals.css('display','');
   };
 
   $(function() {
